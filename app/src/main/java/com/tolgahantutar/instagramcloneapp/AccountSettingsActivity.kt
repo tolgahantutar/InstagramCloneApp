3882830981721102e02af8239bfe2517d9709446
Tolgahan TUTAR <tolgahan.tutar@gmail.com>
2020-11-26 23:23:31 +0300

    User can update profile pic

--- app/src/main/java/com/tolgahantutar/instagramcloneapp/AccountSettingsActivity.kt
+++ app/src/main/java/com/tolgahantutar/instagramcloneapp/AccountSettingsActivity.kt
@@ -1,91 +1,190 @@
 package com.tolgahantutar.instagramcloneapp
 
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Toast
+import com.google.android.gms.tasks.Continuation
+import com.google.android.gms.tasks.OnCompleteListener
+import com.google.android.gms.tasks.Task
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
 import com.google.firebase.database.DataSnapshot
 import com.google.firebase.database.DatabaseError
 import com.google.firebase.database.FirebaseDatabase
 import com.google.firebase.database.ValueEventListener
+import com.google.firebase.storage.FirebaseStorage
+import com.google.firebase.storage.StorageReference
+import com.google.firebase.storage.StorageTask
+import com.google.firebase.storage.UploadTask
 import com.squareup.picasso.Picasso
+import com.theartofdev.edmodo.cropper.CropImage
 import com.tolgahantutar.instagramcloneapp.model.User
 import kotlinx.android.synthetic.main.activity_account_settings.*
-import kotlinx.android.synthetic.main.fragment_profile.view.*
-
+import android.app.ProgressDialog
 class AccountSettingsActivity : AppCompatActivity() {
     private lateinit var firebaseUser: FirebaseUser
     private var checker = ""
+    private var myUrl = ""
+    private var imageUri: Uri? = null
+    private var storageProfilePicRef: StorageReference? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_account_settings)
 
         firebaseUser = FirebaseAuth.getInstance().currentUser!!
-
+        storageProfilePicRef = FirebaseStorage.getInstance().reference.child("Profile Pictures")
         logout_btn.setOnClickListener {
             FirebaseAuth.getInstance().signOut()
 
-            val intent = Intent(this,SigninActivity::class.java)
+            val intent = Intent(this, SigninActivity::class.java)
             intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK)
             startActivity(intent)
             finish()
         }
+        change_image_text_btn.setOnClickListener {
+            checker = "clicked"
+
+            CropImage.activity()
+                .setAspectRatio(1, 1)
+                .start(this@AccountSettingsActivity)
+        }
         save_info_profile_btn.setOnClickListener {
-            if(checker == "clicked")
-            else{
+            if (checker == "clicked") {
+                uploadImageAndUpdateInfo()
+            } else {
                 updateUserInfoOnly()
             }
         }
         getUserInfo()
     }
 
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if (requestCode == CropImage.CROP_IMAGE_ACTIVITY_REQUEST_CODE && resultCode == RESULT_OK && data != null) {
+            val result = CropImage.getActivityResult(data)
+            imageUri = result.uri
+            profile_image_view_profile_frag.setImageURI(imageUri)
+        }
+    }
+
     private fun updateUserInfoOnly() {
         when {
-            full_name_profile_frag.text.toString()=="" -> {
+            full_name_profile_frag.text.toString() == "" -> {
                 Toast.makeText(this, "Please write fullname first", Toast.LENGTH_SHORT).show()
             }
-            username_profile_frag.text.toString()=="" -> {
+            username_profile_frag.text.toString() == "" -> {
                 Toast.makeText(this, "Please write username first", Toast.LENGTH_SHORT).show()
             }
-            bio_profile_frag.text.toString()=="" -> {
+            bio_profile_frag.text.toString() == "" -> {
                 Toast.makeText(this, "Please write bio first", Toast.LENGTH_SHORT).show()
             }
             else -> {
                 val usersRef = FirebaseDatabase.getInstance().reference.child("Users")
-                val userMap = HashMap<String,Any>()
+                val userMap = HashMap<String, Any>()
                 userMap["fullname"] = full_name_profile_frag.text.toString().toLowerCase()
                 userMap["username"] = username_profile_frag.text.toString().toLowerCase()
                 userMap["bio"] = bio_profile_frag.text.toString().toLowerCase()
 
                 usersRef.child(firebaseUser.uid).updateChildren(userMap)
 
-                Toast.makeText(this, "Account information has been updated successfully", Toast.LENGTH_SHORT).show()
+                Toast.makeText(
+                    this,
+                    "Account information has been updated successfully",
+                    Toast.LENGTH_SHORT
+                ).show()
 
-                val intent = Intent(this,MainActivity::class.java)
+                val intent = Intent(this, MainActivity::class.java)
                 startActivity(intent)
                 finish()
-                  }
-           }
+            }
         }
+    }
 
-    private fun getUserInfo(){
-        val usersRef = FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.uid)
+    private fun getUserInfo() {
+        val usersRef =
+            FirebaseDatabase.getInstance().getReference().child("Users").child(firebaseUser.uid)
 
-        usersRef.addValueEventListener(object: ValueEventListener {
+        usersRef.addValueEventListener(object : ValueEventListener {
             override fun onDataChange(snapshot: DataSnapshot) {
-                if(snapshot.exists()){
+                if (snapshot.exists()) {
                     val user = snapshot.getValue<User>(User::class.java)
 
-                    Picasso.get().load(user!!.image).placeholder(R.drawable.profile).into(profile_image_view_profile_frag)
+                    Picasso.get().load(user!!.image).placeholder(R.drawable.profile)
+                        .into(profile_image_view_profile_frag)
                     username_profile_frag.setText(user!!.username)
                     full_name_profile_frag.setText(user!!.fullname)
                     bio_profile_frag.setText(user!!.bio)
                 }
             }
+
             override fun onCancelled(error: DatabaseError) {
             }
         })
+    }
+
+    private fun uploadImageAndUpdateInfo() {
+        when {
+            imageUri == null -> {
+                Toast.makeText(this, "Please select image first", Toast.LENGTH_SHORT).show()
+            }
+            full_name_profile_frag.text.toString() == "" -> {
+                Toast.makeText(this, "Please write full name first", Toast.LENGTH_SHORT).show()
+            }
+            username_profile_frag.text.toString() == "" -> {
+                Toast.makeText(this, "Please write username first", Toast.LENGTH_SHORT).show()
+            }
+            bio_profile_frag.text.toString() == "" -> {
+                Toast.makeText(this, "Please write bio first", Toast.LENGTH_SHORT).show()
+            }
+            else -> {
+                val progressDialog = ProgressDialog(this)
+                progressDialog.setTitle("Account Settings")
+                progressDialog.setMessage("Please wait, we are updating your profile...")
+                progressDialog.show()
+                val fileRef = storageProfilePicRef!!.child(firebaseUser!!.uid + ".jpg")
+
+                var uploadTask: StorageTask<*>
+                uploadTask = fileRef.putFile(imageUri!!)
+
+                uploadTask.continueWithTask(Continuation<UploadTask.TaskSnapshot, Task<Uri>> { task ->
+                    if (!task.isSuccessful) {
+                        task.exception?.let {
+                            throw it
+                            progressDialog.dismiss()
+                        }
+                    }
+                    return@Continuation fileRef.downloadUrl
+                }).addOnCompleteListener (OnCompleteListener<Uri> { task ->
+                    if (task.isSuccessful) {
+                        val downloadUrl = task.result
+                        myUrl = downloadUrl.toString()
+
+                        val ref = FirebaseDatabase.getInstance().reference.child("Users")
+
+                        val userMap = HashMap<String, Any>()
+                        userMap["fullname"] =
+                            full_name_profile_frag.text.toString().toLowerCase()
+                        userMap["username"] =
+                            username_profile_frag.text.toString().toLowerCase()
+                        userMap["bio"] = bio_profile_frag.text.toString().toLowerCase()
+                        userMap["image"] = myUrl
+
+                        ref.child(firebaseUser.uid).updateChildren(userMap)
+                        Toast.makeText(this, "Account information has been updated successfully", Toast.LENGTH_SHORT).show()
+
+                        val intent = Intent(this,MainActivity::class.java)
+                        startActivity(intent)
+                        finish()
+                        progressDialog.dismiss()
+                    }else{
+                        progressDialog.dismiss()
+                    }
+                    }
+                    )
+            }
+        }
     }
 }
--- app/src/main/java/com/tolgahantutar/instagramcloneapp/MainActivity.kt
+++ app/src/main/java/com/tolgahantutar/instagramcloneapp/MainActivity.kt
@@ -1,6 +1,7 @@
 package com.tolgahantutar.instagramcloneapp
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.widget.TextView
 import com.google.android.material.bottomnavigation.BottomNavigationView
@@ -28,6 +29,8 @@ class MainActivity : AppCompatActivity() {
                 return@OnNavigationItemSelectedListener true
             }
             R.id.nav_add_post->{
+                item.isChecked = false
+                startActivity(Intent(this@MainActivity,AddPostActivity::class.java))
                return@OnNavigationItemSelectedListener true
             }
             R.id.nav_notifications->{
